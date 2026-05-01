c333bcf0885971ecaff93a1aeee880f478abf1bc
Tobias Hesterberg <tobiashesterberg3103@gmail.com>
2026-05-01 21:05:54 +0200

    feat: added annotations to Customer DTOs to enhance Swagger endpoint documentation

--- customer-service/src/main/java/de/insureflow/customer_service/DTOs/CustomerRequest.java
+++ customer-service/src/main/java/de/insureflow/customer_service/DTOs/CustomerRequest.java
@@ -1,12 +1,19 @@
 package de.insureflow.customer_service.DTOs;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record CustomerRequest(
+        @Schema(description = "Customers lastname", example = "Doe")
         String surname,
+        @Schema(description = "Customers firstname", example = "John")
         String forename,
+        @Schema(description = "Customers unique email address", example = "john.doe@example.com")
         String emailAddress,
+        @Schema(description = "List of addresses connected to a user")
         List<AddressRequest> address,
+        @Schema(description = "Date of birth", example = "1990-03-22")
         LocalDate birthdate
 ) {}
--- customer-service/src/main/java/de/insureflow/customer_service/DTOs/CustomerResponse.java
+++ customer-service/src/main/java/de/insureflow/customer_service/DTOs/CustomerResponse.java
@@ -1,14 +1,22 @@
 package de.insureflow.customer_service.DTOs;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public record CustomerResponse(
+        @Schema(description = "Customers UUID", example = "6c6207be-5a3b-4546-a231-d238161f5153")
         UUID id,
+        @Schema(description = "Customers lastname", example = "Doe")
         String surname,
+        @Schema(description = "Customers firstname", example = "John")
         String forename,
+        @Schema(description = "Customers unique email address", example = "john.doe@example.com")
         String emailAddress,
+        @Schema(description = "List of addresses connected to a user")
         List<AddressResponse> address,
+        @Schema(description = "Date of birth", example = "1990-03-22")
         LocalDate birthdate) {
 }
