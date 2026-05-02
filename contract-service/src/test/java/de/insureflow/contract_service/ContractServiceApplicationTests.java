430f4f1c8e2bb869087912b0fc6a4e70b43aea81
Tobias Hesterberg <tobiashesterberg3103@gmail.com>
2026-05-02 22:57:58 +0200

    chore: contract-service setup

---
+++ contract-service/src/test/java/de/insureflow/contract_service/ContractServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.insureflow.contract_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
